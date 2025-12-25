a09694d70e0afc28b884797a291da96d37cf5762
Sriharshitha-bodduna <boddunasriharshitha18@gmail.com>
2025-12-25 23:22:58 +0530

    Model

---
+++ User.java
@@ -0,0 +1,36 @@
+package com.codegnan.model;
+
+
+//Model class — represents users table
+
+public class User {
+
+ private String username;
+ private String password;
+ private String email;
+
+ public User() {}
+
+ public User(String username, String password, String email) {
+     this.username = username;
+     this.password = password;
+     this.email = email;
+ }
+
+ public String getUsername() {
+     return username;
+ }
+
+ public String getPassword() {
+     return password;
+ }
+
+ public String getEmail() {
+     return email;
+ }
+
+ @Override
+ public String toString() {
+     return "User [username=" + username + ", email=" + email + "]";
+ }
+}
